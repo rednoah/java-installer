52494a6c824ddc403934f55c4862e7fb9135e578
Reinhard Pointner <reinhard.pointner@gmail.com>
2016-03-24 08:55:18 +0800

    1.8.0_73-b02

--- release/install-jdk.sh
+++ release/install-jdk.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 
-# Unofficial Java Installer for Oracle Java SE 1.8.0_73
+# Unofficial Java Installer for Oracle Java SE 1.8.0_77
 # Example: curl -O https://raw.githubusercontent.com/rednoah/java-installer/master/release/install-jdk.sh && sh -x install-jdk.sh
 
 # JDK version identifiers
 case `uname -m` in
 	armv7l)
-		JDK_URL="http://download.oracle.com/otn-pub/java/jdk/8u73-b02/jdk-8u73-linux-arm32-vfp-hflt.tar.gz"
-		JDK_SHA256="88cdfc96e518cbcf8625d5dde33f0c96b282aae1bc82e8d8e9be8b658fcb4067"
+		JDK_URL="http://download.oracle.com/otn-pub/java/jdk/8u77-b03/jdk-8u77-linux-arm32-vfp-hflt.tar.gz"
+		JDK_SHA256="c3af7a080cd74f7350680a020749b8161f78f66d1f16cda91bf758c3e58b524b"
 	;;
 	armv8)
-		JDK_URL="http://download.oracle.com/otn-pub/java/jdk/8u73-b02/jdk-8u73-linux-arm64-vfp-hflt.tar.gz"
-		JDK_SHA256="dc479344c4af6326d59bed31f9b71c223f20b5c2813295cda343d762d88d2641"
+		JDK_URL="http://download.oracle.com/otn-pub/java/jdk/8u77-b03/jdk-8u77-linux-arm64-vfp-hflt.tar.gz"
+		JDK_SHA256="d194595d0c6d2f0b6fad36da489e26a65e3825af85c051ce3c84d5a1f5a96b31"
 	;;
 	i686)
-		JDK_URL="http://download.oracle.com/otn-pub/java/jdk/8u73-b02/jdk-8u73-linux-i586.tar.gz"
-		JDK_SHA256="3beda1742719400bc493cf118fc3c71697971cec3ae95a2430a337eb4d143dda"
+		JDK_URL="http://download.oracle.com/otn-pub/java/jdk/8u77-b03/jdk-8u77-linux-i586.tar.gz"
+		JDK_SHA256="f2889b778538fe3d20b2d37d0de3c3204de86126aadb75c1bb38d11255b41e92"
 	;;
 	x86_64)
-		JDK_URL="http://download.oracle.com/otn-pub/java/jdk/8u73-b02/jdk-8u73-linux-x64.tar.gz"
-		JDK_SHA256="46a8149efaf9d87a147b244e503f1d7791ec8abbc35a5011c99ed909edb2e3e6"
+		JDK_URL="http://download.oracle.com/otn-pub/java/jdk/8u77-b03/jdk-8u77-linux-x64.tar.gz"
+		JDK_SHA256="a47dc0962a57b27a0cc00b9f11a53dc3add40c98633ba49a2419b845e4dedf43"
 	;;
 	*)
-		echo "CPU architecture not supported: `uname -m`"
+		echo "Unkown CPU architecture: `uname -m`"
 		exit 1
 	;;
 esac
@@ -45,8 +45,8 @@ fi
 echo "Extract $JDK_TAR_GZ"
 tar -v -zxf "$JDK_TAR_GZ"
 
-# find latest java executable
-JAVA_EXE=`find "$PWD" -name "java" -type f -print0 | xargs -0 ls -Alt1 | head -n 1  | sed -e 's/\s\+/\s /g' | cut -d' ' -f7-`
+# find java executable
+JAVA_EXE=`find "$PWD" -name "java" -type f | head -n 1`
 
 # link executable into /usr/local/bin/java
 mkdir -p "/usr/local/bin"
