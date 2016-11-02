a0b982df108187003d50c36b037d08619973aaa6
Reinhard Pointner <reinhard.pointner@gmail.com>
2016-11-03 03:39:02 +0800

    Play with PowerShell

--- src/install-jre.ps1
+++ src/install-jre.ps1
@@ -7,7 +7,7 @@ $JDK_SHA256 = "@{jre.windows.x64.sha256}"
 
 # fetch JDK
 $JDK_TAR_GZ = Split-Path -Leaf $JDK_URL
-echo "Download $JDK_TAR_GZ"
+Write-Output "Download $JDK_TAR_GZ"
 
 if (!(test-path $JDK_TAR_GZ)) {
 	$session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
@@ -16,16 +16,16 @@ if (!(test-path $JDK_TAR_GZ)) {
 	$cookie.Value = "accept-securebackup-cookie"
 	$cookie.Domain = "oracle.com"
 	$session.Cookies.Add($cookie)
-	Invoke-RestMethod -ContentType "application/octet-stream" -WebSession $session -Uri $JDK_URL  -OutFile $JDK_TAR_GZ
+	Invoke-WebRequest -WebSession $session -Uri $JDK_URL -OutFile $JDK_TAR_GZ
 }
 
 # verify archive via SHA-256 checksum
 $JDK_SHA256_ACTUAL = (Get-FileHash -Algorithm SHA256 $JDK_TAR_GZ).hash.toLower()
-echo "Expected SHA256 checksum: $JDK_SHA256"
-echo "Actual SHA256 checksum: $JDK_SHA256_ACTUAL"
+Write-Output "Expected SHA256 checksum: $JDK_SHA256"
+Write-Output "Actual SHA256 checksum: $JDK_SHA256_ACTUAL"
 
 if ($JDK_SHA256 -ne $JDK_SHA256_ACTUAL) {
 	throw "ERROR: SHA256 checksum mismatch"
 }
 
-echo "OK"
+Write-Output "OK"
