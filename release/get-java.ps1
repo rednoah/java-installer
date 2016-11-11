c10730b5c48d76becc4d726b4d5930f998d05860
Reinhard Pointner <reinhard.pointner@gmail.com>
2016-11-11 23:21:09 +0800

    Refactor sh / ps1 scripts and support install / fetch actions (useful for automated builds)

---
+++ release/get-java.ps1
@@ -0,0 +1,78 @@
+# Unofficial Java Installer for Oracle Java SE 1.8.0_111
+# Example: Invoke-WebRequest https://raw.githubusercontent.com/rednoah/java-installer/master/release/install-jre.ps1 | Invoke-Expression
+
+
+$ErrorActionPreference = "Stop"
+
+
+# JDK version identifiers
+$JDK_ARCH = "$ENV:PROCESSOR_ARCHITECTURE"
+
+Switch ($JDK_ARCH) {
+	AMD64 {
+		$JDK_URL = "http://download.oracle.com/otn-pub/java/jdk/8u111-b14/jre-8u111-windows-x64.tar.gz"
+		$JDK_SHA256 = "60140fa25ecb80825b74bbb428e99278f7c2820de15118d6d7e4d0ed737b6d60"
+	}
+	x86 {
+		$JDK_URL = "http://download.oracle.com/otn-pub/java/jdk/8u111-b14/jre-8u111-windows-i586.tar.gz"
+		$JDK_SHA256 = "9b039bcb5a78d850fb5bea5ce3cb47ae98d7c58f3e61714bd39050f206138b68"
+	}
+	default {
+		throw "CPU architecture not supported: $JDK_ARCH"
+	}
+}
+
+
+# fetch JDK
+$JDK_TAR_GZ = Split-Path -Leaf $JDK_URL
+
+if (!(test-path $JDK_TAR_GZ)) {
+	Write-Output "Download $JDK_TAR_GZ"
+	$session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+	$cookie = New-Object System.Net.Cookie 
+	$cookie.Name = "oraclelicense"
+	$cookie.Value = "accept-securebackup-cookie"
+	$cookie.Domain = "oracle.com"
+	$session.Cookies.Add($cookie)
+	Invoke-WebRequest -WebSession $session -Uri $JDK_URL -OutFile $JDK_TAR_GZ
+}
+
+
+# verify archive via SHA-256 checksum
+$JDK_SHA256_ACTUAL = (Get-FileHash -Algorithm SHA256 $JDK_TAR_GZ).hash.toLower()
+Write-Output "Expected SHA256 checksum: $JDK_SHA256"
+Write-Output "Actual SHA256 checksum: $JDK_SHA256_ACTUAL"
+
+if ($JDK_SHA256 -ne $JDK_SHA256_ACTUAL) {
+	throw "ERROR: SHA256 checksum mismatch"
+}
+
+
+# extract and link only if explicitly requested
+if ("$1" -ne "install") {
+	Write-Output "Download complete: $JDK_TAR_GZ"
+	return
+}
+
+
+# use 7-Zip to extract tar
+Write-Output "Extract $JDK_TAR_GZ"
+& 7z e -aos $JDK_TAR_GZ
+& 7z x -aos ([System.IO.Path]::GetFileNameWithoutExtension($JDK_TAR_GZ))
+
+
+# find java executable
+$JAVA_EXE = Get-ChildItem -recurse -include java.exe | Sort-Object LastWriteTime | Select-Object -ExpandProperty FullName -Last 1
+
+# test
+Write-Output "Execute ""$JAVA_EXE"" -XshowSettings -version"
+& $JAVA_EXE -XshowSettings -version
+
+
+# set %JAVA_HOME% and add java to %PATH%
+$JAVA_HOME = Split-Path -Parent (Split-Path -Parent $JAVA_EXE)
+
+Write-Output "`nPlease add JAVA_HOME\bin to the PATH if you have not done so already:"
+Write-Output "`n`t%JAVA_HOME%\bin"
+Write-Output "`nPlease set JAVA_HOME:"
+Write-Output "`n`tsetx JAVA_HOME ""$JAVA_HOME"""
---
+++ release/get-java.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+
+# Unofficial Java Installer for Oracle Java SE 1.8.0_111
+# Example: curl -O https://raw.githubusercontent.com/rednoah/java-installer/master/release/get-java.sh && sh -x get-java.sh install
+
+
+# JDK version identifiers
+JDK_ARCH=`uname -sm`
+
+case "$JDK_ARCH" in
+	"Linux armv7l")
+		JDK_URL="http://download.oracle.com/otn-pub/java/jdk/8u111-b14/jdk-8u111-linux-arm32-vfp-hflt.tar.gz"
+		JDK_SHA256="add3f9685161337fd31e433ea9b19231d6b40561dc40826057930fa2d76d7925"
+	;;
+	"Linux armv8")
+		JDK_URL="http://download.oracle.com/otn-pub/java/jdk/8u111-b14/jdk-8u111-linux-arm64-vfp-hflt.tar.gz"
+		JDK_SHA256="8fd6be278b1d312c53897de66e9bd2e6eb5ce0d5b62da8bbd9b1333dc190ed15"
+	;;
+	"Linux i686")
+		JDK_URL="http://download.oracle.com/otn-pub/java/jdk/8u111-b14/jdk-8u111-linux-i586.tar.gz"
+		JDK_SHA256="9a8e8fcf7b6fbe53d6312195be87ce00f3beaab9dfbaa020b96e305174f39e1f"
+	;;
+	"Linux x86_64")
+		JDK_URL="http://download.oracle.com/otn-pub/java/jdk/8u111-b14/jdk-8u111-linux-x64.tar.gz"
+		JDK_SHA256="187eda2235f812ddb35c352b5f9aa6c5b184d611c2c9d0393afb8031d8198974"
+	;;
+	"Darwin x86_64")
+		JDK_URL="http://download.oracle.com/otn-pub/java/jdk/8u111-b14/jre-8u111-macosx-x64.tar.gz"
+		JDK_SHA256="2683617c1a508d70c4dbb1305e83857093606eac00f28c25c468cdd5a52178df"
+	;;
+	*)
+		echo "Architecture not supported: $JDK_ARCH"
+		exit 1
+	;;
+esac
+
+
+# fetch JDK
+JDK_TAR_GZ=`basename $JDK_URL`
+if [ ! -f "$JDK_TAR_GZ" ]; then
+	echo "Download $JDK_URL"
+	curl -v -L -o "$JDK_TAR_GZ" --retry 5 --cookie "oraclelicense=accept-securebackup-cookie" "$JDK_URL"
+fi
+
+
+# verify archive via SHA-256 checksum
+JDK_SHA256_ACTUAL=`openssl dgst -sha256 -hex "$JDK_TAR_GZ" | egrep --only-matching "[a-f0-9]{64}"`
+echo "Expected SHA256 checksum: $JDK_SHA256"
+echo "Actual SHA256 checksum: $JDK_SHA256_ACTUAL"
+
+if [ "$JDK_SHA256" != "$JDK_SHA256_ACTUAL" ]; then
+	echo "ERROR: SHA256 checksum mismatch"
+	exit 1
+fi
+
+
+# extract and link only if explicitly requested
+if [ "$1" != "install" ]; then
+	echo "Download complete: $JDK_TAR_GZ"
+	exit 0
+fi
+
+
+echo "Extract $JDK_TAR_GZ"
+tar -v -zxf "$JDK_TAR_GZ"
+
+# find java executable
+JAVA_EXE=`find "$PWD" -name "java" -type f | head -n 1`
+
+# link executable into /usr/local/bin/java
+mkdir -p "/usr/local/bin"
+ln -s -f "$JAVA_EXE" "/usr/local/bin/java"
+
+# link java home to /usr/local/java
+JAVA_BIN=`dirname $JAVA_EXE`
+JAVA_HOME=`dirname $JAVA_BIN`
+ln -s -f "$JAVA_HOME" "/usr/local/java"
+
+# test
+echo "Execute $JAVA_EXE -XshowSettings -version"
+"$JAVA_EXE" -XshowSettings -version
