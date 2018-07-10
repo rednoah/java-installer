e330c064f3411aacbeca296ea8c825bd1370fd4b
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-07-10 12:14:54 +0700

    /usr/local seems to be quite ephemeral on QNAP...

--- package/qnap/shared/start-stop-status.sh
+++ package/qnap/shared/start-stop-status.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 CONF="/etc/config/qpkg.conf"
 QPKG_NAME="oracle-java"
-QPKG_ROOT=`/sbin/getcfg $QPKG_NAME Install_Path -f $CONF`
+QPKG_ROOT=$(/sbin/getcfg $QPKG_NAME Install_Path -f $CONF)
 
 
 QPKG_LOG="$QPKG_ROOT/install-jdk.log"
@@ -17,55 +17,27 @@ case "$1" in
 			exit 1
 		fi
 
-		# find java executable
-		JAVA_EXE=`find "$QPKG_ROOT" -name "java" -type f | head -n 1`
-		JAVA_BIN=`dirname $JAVA_EXE`
-		JAVA_HOME=`dirname $JAVA_BIN`
+		JAVA_EXE=$QPKG_ROOT/*/bin/java
+		JAVA_BIN=$(dirname $JAVA_EXE)
+		JAVA_HOME=$(dirname $JAVA_BIN)
 
-		# link executable into /usr/local/bin/java
-		ln -sf "$JAVA_EXE" "/usr/bin/java"
-
-		# link java home to /usr/local/java
-		ln -sf "$JAVA_HOME" "/usr/share/java"
-
-		# make sure that `java` is working
-		if [ -x "/usr/bin/java" ]; then
-			# display success message
-			"/usr/bin/java" -version >> "$QPKG_LOG" 2>&1
-		else
-			# display error message
-			err_log "Ooops, something went wrong... View Log for details... $QPKG_LOG"
-		fi
-
-		# add JAVA_HOME to system-wide profile
-		if [ `grep -c "$COMMENT" $SYS_PROFILE` == "0" ]; then
-			if [ -x "/usr/share/java/bin/java" ]; then
-				echo "Add environment variables to $SYS_PROFILE" >> "$QPKG_LOG"
-
-				# add environment variables to /etc/profile
-				echo "export JAVA_HOME=/usr/share/java    $COMMENT" >> "$SYS_PROFILE"
-				echo "export LANG=en_US.utf8              $COMMENT" >> "$SYS_PROFILE"
-			fi
-		fi
-	;;
+		/bin/ln -sf "$JAVA_EXE" "/usr/bin/java"
+		/bin/ln -sf "$JAVA_HOME" "/opt/java"
+		;;
 
 	stop)
-		# remove symlinks
-		rm "/usr/bin/java"
-		rm "/usr/share/java"
-
-		# remove /etc/profile additions
-		sed -i "/${COMMENT}/d" "$SYS_PROFILE"
-	;;
+		rm -rf "/usr/bin/java"
+		rm -rf "/opt/java"
+		;;
 
 	restart)
 		$0 stop
 		$0 start
-	;;
+		;;
 
 	*)
-	echo "Usage: $0 {start|stop|restart}"
-	exit 1
+		echo "Usage: $0 {start|stop|restart}"
+		exit 1
 esac
 
 exit 0
