b727dc0e20c0d29cac9c1cfee8c24fbb8808139c
Reinhard Pointner <reinhard.pointner@gmail.com>
2018-10-22 02:27:41 +0700

    Gluon JavaFX

--- build-jdk.groovy
+++ build-jdk.groovy
@@ -14,23 +14,31 @@ def openjdk = [
 
 // BellSoft Liberica JDK Linux ARM
 def liberica = [
-	[os: 'linux', arch: 'aarch64', pkg: 'linux-aarch64-lite.tar.gz'],
-	[os: 'linux', arch: 'armv7l', pkg: 'linux-arm32-vfp-hflt-lite.tar.gz']
+	[os: 'linux',   arch: 'aarch64', pkg: 'linux-aarch64-lite.tar.gz'],
+	[os: 'linux',   arch: 'armv7l',  pkg: 'linux-arm32-vfp-hflt-lite.tar.gz']
 ]
 
 
 // Gluon JavaFX
 def javafx = [
-	[os: 'windows', arch: 'x64'],
-	[os: 'mac',     arch: 'x64']
-	[os: 'linux',   arch: 'x64']
+	[os: 'windows', arch: 'x64', pkg: 'windows-x64_bin-sdk.zip'],
+	[os: 'mac',     arch: 'x64', pkg: 'osx-x64_bin-sdk.zip'],
+	[os: 'linux',   arch: 'x64', pkg: 'linux-x64_bin-sdk.zip']
 ]
+
+
+
+def sha256(url) {
+	def file = new File('cache', url.tokenize('/').last())
+	new AntBuilder().get(src: url, dest: file, skipExisting: 'yes')
+	return file.bytes.digest('SHA-256')
+}
 
 
 // generate properties file
 ant.propertyfile(file: 'build-jdk.properties', comment: "${name} ${version} binaries") {
-	entry(key:"jdk.name", value: name)
-	entry(key:"jdk.version", value: version)
+	entry(key: 'jdk.name', value: name)
+	entry(key: 'jdk.version', value: version)
 
 	openjdk.each{ jdk ->
 		jdk.with {
@@ -45,26 +53,20 @@ ant.propertyfile(file: 'build-jdk.properties', comment: "${name} ${version} bina
 	liberica.each{ jdk ->
 		jdk.with {
 			def url = "https://github.com/bell-sw/Liberica/releases/download/${major}/bellsoft-jdk${major}-${pkg}"
-
-			// GitHub Release API doesn't give us the SHA-256
-			println "Download $url"
-			def checksum = new URL(url).bytes.digest('SHA-256')
+			def checksum = sha256(url)
 
 			entry(key:"jdk.${os}.${arch}.url", value: url)
 			entry(key:"jdk.${os}.${arch}.sha256", value: checksum)
 		}
 	}
 
-
 	javafx.each{ jfx ->
 		jfx.with {
-			def url = "http://gluonhq.com/download/javafx-${major}-sdk-${os}/"
-
-			println "Download $url"
-			def checksum = new URL(url).bytes.digest('SHA-256')
+			def url = "http://download2.gluonhq.com/openjfx/${major}/openjfx-${major}_${pkg}"
+			def checksum = sha256(url)
 
 			entry(key:"jfx.${os}.${arch}.url", value: url)
 			entry(key:"jfx.${os}.${arch}.sha256", value: checksum)
 		}
-	}	
+	}
 }
